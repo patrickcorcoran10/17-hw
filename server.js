42366b03b239e7cfbd448cc6a95b2df78523d915
Patrick Corcoran <patrickcorcoran@Patricks-MacBook-Pro.local>
2021-02-09 19:54:25 -0600

    commit

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 app.use(logger("dev"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
